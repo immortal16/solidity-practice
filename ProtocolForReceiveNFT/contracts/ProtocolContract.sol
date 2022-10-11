1b78dd08f4b79434b51088e0b4bba7f9b5c66e14
immortal16 <zhenyav20133@gmail.com>
2022-10-11 15:32:51 +0300

    events & transfer fixed

--- ProtocolForReceiveNFT/contracts/ProtocolContract.sol
+++ ProtocolForReceiveNFT/contracts/ProtocolContract.sol
@@ -13,14 +13,14 @@ contract Protocol is IERC721Receiver {
     address private signer;
     uint8 public nftId;
 
-    event deposited(uint8 tokenId);
-    event withdrawed(uint8 tokenId);
+    event Deposit(uint8 tokenId);
+    event Withdraw(uint8 tokenId);
 
     constructor(address _verifySignatureContractAddress) {
         verifySignatureContract = _verifySignatureContractAddress;
     }
 
-    function transfer(address to, uint256 tokenId) public {
+    function transfer(address to, uint256 tokenId) private {
         IERC721(nftContract).safeTransferFrom(address(this), to, tokenId);
     }
 
@@ -30,17 +30,16 @@ contract Protocol is IERC721Receiver {
         nftContract = _nftContractAddress;
 
         IERC721(nftContract).safeTransferFrom(msg.sender, address(this), id);
-
-        emit deposited(id);
+        emit Deposit(id);
     }
 
     function withdrawNFT(bytes32 _ethSignedMessageHash, bytes memory _signature) external {
         require(nftId != 0 && IERC721(nftContract).ownerOf(nftId) == address(this), "Requested nft is not available yet.");
         address _signer = IVerifySignature(verifySignatureContract).recoverSigner(_ethSignedMessageHash, _signature);
         require(_signer == signer, "Invalid signature, transfer can not be proceeded.");
+        
         transfer(msg.sender, nftId);
-
-        emit withdrawed(nftId);
+        emit Withdraw(nftId);
     }
 
     function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
